33d4ca5bce94b88428ccf6f9cfe0c925141c0e54
JeyyGit <20415821+JeyyGit@users.noreply.github.com>
2024-05-12 02:44:29 +0700

    fix

--- lib/presentation/user/home.dart
+++ lib/presentation/user/home.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -66,10 +65,10 @@ class Home extends StatelessWidget {
                                       fontWeight: FontWeight.bold,
                                       color: Colors.white),
                                 ),
-                                FutureBuilder<Map<String, dynamic>?>(
+                                FutureBuilder<Map<String, dynamic>>(
                                     future: SaveData.getAuth(),
                                     builder: (BuildContext context,
-                                        AsyncSnapshot<Map<String, dynamic>?>
+                                        AsyncSnapshot<Map<String, dynamic>>
                                             snapshot) {
                                       if (snapshot.connectionState ==
                                           ConnectionState.waiting) {
