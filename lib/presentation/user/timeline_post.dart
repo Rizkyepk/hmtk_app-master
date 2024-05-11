7706f97c855276c10187b7f96953130b2ef1ce22
JeyyGit <20415821+JeyyGit@users.noreply.github.com>
2024-05-12 04:24:24 +0700

    add timeline post image preview

--- lib/presentation/user/timeline_post.dart
+++ lib/presentation/user/timeline_post.dart
@@ -23,8 +23,10 @@ class _TimelinePostState extends State<TimelinePost> {
     final ImagePicker picker = ImagePicker();
     final XFile? imagePicked =
         await picker.pickImage(source: ImageSource.gallery);
-    image = File(imagePicked!.path);
-    // setState(() {});
+    // image = File(imagePicked!.path);
+    setState(() {
+      image = File(imagePicked!.path);
+    });
   }
 
   @override
@@ -33,8 +35,11 @@ class _TimelinePostState extends State<TimelinePost> {
       body: MyPage(
           widget: Column(
         children: [
-          Expanded(
-              flex: 1,
+          // Expanded(
+          Container(
+              // flex: 1,
+              height: 58,
+              margin: const EdgeInsets.only(top: 30),
               child: Padding(
                 padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
                 child: Row(
@@ -81,10 +86,11 @@ class _TimelinePostState extends State<TimelinePost> {
                 ),
               )),
           Expanded(
-            flex: 5,
+            // flex: 5,
             child: Container(
               padding: const EdgeInsets.all(15),
-              margin: const EdgeInsets.only(left: 20, right: 20),
+              // margin: const EdgeInsets.only(left: 20, right: 20),
+              margin: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20), color: Colors.white),
               child: const TextField(
@@ -96,59 +102,100 @@ class _TimelinePostState extends State<TimelinePost> {
               ),
             ),
           ),
-          Expanded(
-              flex: 1,
+          // Expanded(
+          Container(
+              // flex: 1,
               child: Padding(
-                padding: const EdgeInsets.only(
-                    top: 20, bottom: 5, left: 20, right: 20),
-                child: Column(
-                  children: [
-                    InkWell(
-                      onTap: () async {
-                        await getImage();
-                      },
-                      child: const Row(
-                        children: [
-                          Icon(
-                            Icons.cloud_upload_outlined,
-                            size: 30,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            'Unggah Gambar',
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          ),
-                        ],
-                      ),
+            padding:
+                const EdgeInsets.only(top: 5, bottom: 5, left: 20, right: 20),
+            child: Column(
+              children: [
+                if (image != null)
+                  Stack(children: [
+                    Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: SizedBox(
+                                height: 200,
+                                width: double.infinity,
+                                child: Image.file(image!, fit: BoxFit.cover)))),
+                    Positioned(
+                      top: 10, // Adjust top position as needed
+                      right: 10, // Adjust right position as needed
+                      child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              image = null;
+                            });
+                          },
+                          child: Container(
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black
+                                        .withOpacity(0.2), // Shadow color
+                                    spreadRadius: 2, // Spread radius
+                                    blurRadius: 5, // Blur radius
+                                    offset: Offset(0, 2), // Offset
+                                  ),
+                                ],
+                                shape: BoxShape
+                                    .circle, // Optional: You can change the shape as needed
+                              ),
+                              child: Icon(
+                                Icons.close,
+                                color: Colors.white,
+                                size: 40,
+                              ))),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          'Komentar',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ),
-                        Switch(
-                            value: nonActiveComentar,
-                            onChanged: (value) {
-                              setState(() {
-                                nonActiveComentar = value;
-                              });
-                            })
-                      ],
-                    )
-                  ],
+                  ]),
+                InkWell(
+                  onTap: () async {
+                    await getImage();
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.cloud_upload_outlined,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Unggah Gambar',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
-              ))
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Komentar',
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Switch(
+                        value: nonActiveComentar,
+                        onChanged: (value) {
+                          setState(() {
+                            nonActiveComentar = value;
+                          });
+                        })
+                  ],
+                )
+              ],
+            ),
+          ))
         ],
       )),
     );
